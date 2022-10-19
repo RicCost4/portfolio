2abd3b7424e6c8f310f0059f413950a4988a587f
Richard Costa <richardarcosta@hotmail.com>
2022-10-19 20:32:02 -0300

    Update Ambiente Dev 19/10

--- js/app.js
+++ js/app.js
@@ -3,4 +3,10 @@ function carregar(){
     let ano = new Date()
     let year = ano.getFullYear()
     data.innerHTML = `2022 - ${year}`
+
+}
+function mostrar(div){
+    var divs = document.getElementById(div)
+    var disp = divs.style.display
+    divs.style.display = disp == 'none' ? 'block' : 'none'
 }
