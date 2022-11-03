79c8a7324487035d17c12db3c7cc22bfdacb1a74
Richard Costa <richardarcosta@hotmail.com>
2022-11-03 09:49:32 -0300

    Update script_projeto.js

--- js/script_projeto.js
+++ js/script_projeto.js
@@ -1,13 +1,12 @@
 function mostrar(div, btn){
     let divs = document.getElementById(div)
     let elem = document.getElementById(btn)
-    let disp = divs.style.display
 
-    if(disp == 'none'){
-        elem.innerHTML = '&#8593;'
-        divs.style.display = 'block'
-    } else{
+    if(divs.style.display == 'block'){
         elem.innerHTML = '&#8595;'
         divs.style.display = 'none'
+    } else{
+        elem.innerHTML = '&#8593;'
+        divs.style.display = 'block'
     }
 }
